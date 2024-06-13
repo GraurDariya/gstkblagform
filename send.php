e4a13b5277c9a7632c5e1daf3f9dbc7dfeff57c2
GraurDariya <167690419+GraurDariya@users.noreply.github.com>
2024-06-13 23:32:56 +0300

    send.php

--- send.php
+++ send.php
@@ -1,21 +1,28 @@
 <?php
-if($_SERVER["REQUEST_METHOD"] == "POST") {
-    $selectOption1 = $_POST['select-option'];
-    $fullName = $_POST['fullinn'];
-    $email = $_POST['email'];
-    $phone = $_POST['phone'];
-    $selectOption2 = $_POST['select-option-2'];
-    $fullUsluga = $_POST['fullusluga'];
-    
-    $to = 'dariya.5416@gmail.com'; // Ваш email
-    $subject = 'Новая заявка на услугу';
-    $message = "Укажите лицо: $selectOption1\nФИО: $fullName\nEmail: $email\nТелефон: $phone\nВыбранный раздел услуг: $selectOption2\nПодробнее об услуге: $fullUsluga";
-    $headers = 'From: ' . $email;
 
-    if (mail($to, $subject, $message, $headers)) { 
-        echo 'Ваша заявка успешно отправлена.'; 
-    } else { 
-        echo 'При отправке заявки произошла ошибка. Код ошибки: ' . error_get_last()['message']; 
-    }     
-}
+  $select-option-1=$_POST['select-option'];
+  $fullname=$_POST['fullname'];
+  $fullinn=$_POST['fullinn'];
+  $email=$_POST['email'];
+  $phone=$_POST['phone'];
+  $select-option-2=$_POST['select-option'];
+  $fullusluga=$_POST['fullusluga'];
+
+  $to="dariya.5416@gmail.com";
+  $date=date("d.m.Y");
+  $time=date("h:i");
+  $from=$email;
+  $subject="Заявка с сайта";
+
+  $msg="
+  Лицо:$select-option-1 /n
+  ФИО:$fullname /n
+  ИНН:$fullinn /n
+  Email:$email /n
+  Телефон:$phone /n
+  Раздел услуг:$select-option-2 /n
+  Подробнее об услуге:$fullusluga";
+  mail($to, $subject, $msg, "From: $from");
+
 ?>
+
