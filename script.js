ec80e2fa3c40040b95505aa586d00ad5a73f93c6
GraurDariya <167690419+GraurDariya@users.noreply.github.com>
2024-06-13 22:47:56 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
-document.getElementById('application-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    var fullname = document.getElementById('fullname').value;
-    var email = document.getElementById('email').value;
-    var phone = document.getElementById('phone').value;
-    var fullinn = document.getElementById('fullinn').value;
-    var fullusluga = document.getElementById('fullusluga').value;
-    alert('ФИО: ' + fullname + '\nEmail: ' + email + '\nТелефон: ' + phone + '\nИНН: ' + fullinn+ '\nУслуга: ' + fullusluga);
-});
+JQuery('form button').click(function(){
+    var form = JQuery(this).closest('form');
+
+    if(form.valid()){
+        var actUrl=form.attr('action');
+
+        JQuery.ajax({
+            url:actUrl,
+            type:'post',
+            dataType:'html',
+            data:form.serializeO(),
+            success:function(data){
+                form.find('.status').html('Заявка успешно отправлена');
+            };
+            error:    function(){
+                form.find('.status').html('серверная ошибка');
+            }
+    });
+}
+                            });
