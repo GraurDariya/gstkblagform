5312ecd506233ff5682c14382abf99095917ee4f
GraurDariya <167690419+GraurDariya@users.noreply.github.com>
2024-06-13 22:08:19 +0300

    Update send.php

--- send.php
+++ send.php
@@ -1,4 +1,4 @@
-<php
+<?php
 
   $select-option-1=$_POST['select-option'];
   $fullname=$_POST['fullname'];
@@ -8,44 +8,19 @@
   $select-option-2=$_POST['select-option'];
   $fullusluga=$_POST['fullusluga'];
 
-  $select-option-1=htmlspecialcharts($select-option);
-  $fullname=htmlspecialcharts($fullname);
-  $fullinn=htmlspecialcharts($fullinn);
-  $email=htmlspecialcharts($email);
-  $phone=htmlspecialcharts($phone);
-  $select-option-2=htmlspecialcharts($select-option);
-  $fullusluga=htmlspecialcharts($fullusluga);
+  $to="dariya.5416@gmail.com";
+  $date=date("d.m.Y");
+  $time=date("h:i");
+  $subject="Заявка с сайта";
 
-$select-option-1=urldecode($select-option);
-  $fullname=urldecode($fullname);
-  $fullinn=urldecode($fullinn);
-  $email=urldecode($email);
-  $phone=urldecode($phone);
-  $select-option-2=urldecode($select-option);
-  $fullusluga=urldecode($fullusluga);
+  $msg="
+  Лицо:$select-option-1 /n
+  ФИО:$fullname /n
+  ИНН:$fullinn /n
+  Email:$email /n
+  Телефон:$phone /n
+  Раздел услуг:$select-option-2 /n
+  Подробнее об услуге:$fullusluga";
+  mail($to, $subject, $msg, "From: $From");
 
-$select-option-1=trim($select-option);
-  $fullname=trim($fullname);
-  $fullinn=trim($fullinn);
-  $email=trim($email);
-  $phone=trim($phone);
-  $select-option-2=trim($select-option);
-  $fullusluga=trim($fullusluga);
-
-
-if (mail("dariya.5416@gmail.com",
-          "Новое письмо с сайта",
-          "Лицо: ".$select-option-1."\n".
-          "ФИО: ".$fullname."\n".
-          "ИНН: ".$fullinn."\n".
-          "Email: ".$email."\n".
-          "Телефон: ".$phone."\n".
-          "Раздел услуг: ".$select-option-2."\n".
-          "Подробнее об услуге: ".fullusluga."\n".
-){
-    echo('Письмо успешно отправлено!');
-}
-else {
-echo('Есть ошибки! Проверьте данные...');
-}
-?php>
+?>
